5d129e1b4176951cab51392816738410d6ece333
Amartya Datta Gupta <amartya00@gmail.com>
2021-07-25 22:19:36 -0400

    Version upgrade.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="thesoup",
-    version="0.4",
+    version="0.5",
     description="A soup of random python utils",
     long_description=long_description,
     long_description_content_type="text/markdown",
